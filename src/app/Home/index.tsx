46ae9f23386a32895f0d835c31945074ad024d3d
murilodamarioo <murilo.gcdamario@gmail.com>
2025-05-27 21:57:56 -0300

    :sparkles: feat: Create Input component

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -2,13 +2,15 @@ import { View, Image } from 'react-native'
 import { styles } from './styles'
 
 import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo}/>
 
-      <Button title="Entrar"/>
+      <Input placeholder="O que você precisa comprar?" />
+      <Button title='Adicionar'/>
     </View>
   )
 }
