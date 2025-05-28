668a973ff1fe5673edae98fc5bcc25eff77c0170
murilodamarioo <murilo.gcdamario@gmail.com>
2025-05-27 22:07:27 -0300

    :sparkles: feat: Implements styles to the form and content

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -9,8 +9,14 @@ export function Home() {
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo}/>
 
-      <Input placeholder="O que você precisa comprar?" />
-      <Button title='Adicionar'/>
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title='Adicionar'/>
+      </View>
+
+      <View style={styles.content}>
+        
+      </View>
     </View>
   )
 }
